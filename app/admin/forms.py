63bb2507a85833a0fb48904f7d6340cbf5eb5399
KaijianYou <kaijianyou@gmail.com>
2017-05-01 17:58:33 +0800

    Add module: admin

---
+++ app/admin/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+
+from ..models import User, Role, Post, Category, Comment
+
+
---
+++ app/admin/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+from flask import request
+from flask import url_for
+
+from . import admin
+
